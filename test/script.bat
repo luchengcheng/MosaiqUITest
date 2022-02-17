087fa153a2d89587002962038fe647fecc063f9e
Lu <chengcheng.lu@elekta.com>
2022-02-17 16:21:35 +0800

    update script.bat

--- test/script.bat
+++ test/script.bat
@@ -1,4 +1,9 @@
 @echo off
 setlocal EnableDelayedExpansion
-echo current path is  !cd!
-copy C:\Program Files (x86)\Windows Application Driver\WinAppDriver.exe C:\MOSAIQ\2022.10\2022.10.032\IMPAC\MOSAIQ_APP
+SET WinAppDriver_EXEFILE=C:\Program Files (x86)\Windows Application Driver\WinAppDriver.exe
+SET MOSAIQ_INSTALL_PATH=C:\MOSAIQ\2022.10\2022.10.032\IMPAC\MOSAIQ_APP
+if exist %WinAppDriver_EXEFILE%
+	echo 'found WinAppDriver exe file'
+if exist %MOSAIQ_INSTALL_PATH%
+	echo 'found Mosaiq installation file'
+copy WinAppDriver_EXEFILE  MOSAIQ_INSTALL_PATH
